8a4cd5868841e17771cd0985e5a9a1651bea981e
Shofikul2024 <islamshofi942@gmail.com>
2025-01-20 00:55:04 +0600

    added for of

---
+++ array-check.js
@@ -0,0 +1,5 @@
+const flowers=" golap ,joba ,beli " ;
+const convert =flowers .split(" ,")
+console.log(convert)
+console.log( Array.isArray (convert))
+
---
+++ for-of.js
@@ -0,0 +1,5 @@
+const flowers =[ 'golap' , ' sapla' , ' joba ', ' beli'] ;
+ for ( let item of flowers ) {
+console.log(item)
+
+ }
