0231ce69925d9a0a89a5af8d2051b9cb7848730a
Shofikul2024 <islamshofi942@gmail.com>
2025-01-20 11:35:41 +0600

    added object

---
+++ object.js
@@ -0,0 +1,13 @@
+const person ={
+name : 'shofiukl'  ,
+address : 'jamalpur'  ,
+
+}
+
+function createEmail ( info)  {
+
+const result =info.name +'@'  + info.address +'.com'
+
+return result 
+}
+ console.log ( createEmail (person ))
